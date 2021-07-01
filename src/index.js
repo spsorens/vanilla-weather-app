84214359ecc2e712d262b204bb16558bf602b4ed
spsorens <spsorens@gmail.com>
2021-06-30 22:00:42 -0700

    Celsius Link
    Added Celsius conversion, linked Farenheit and Celsius. Also added in humidity and wind speed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//display date&time
 function formatDate(date) {
   let hour = now.getHours();
   if (hour < 10) {
@@ -24,14 +25,23 @@ function formatDate(date) {
 let today = document.querySelector("#date-time");
 let now = new Date();
 today.innerHTML = formatDate(now);
-let iconElement = document.querySelector("#icon");
 
+//show weather
 function showTemp(response) {
-  console.log(response.data);
-  document.querySelector("#city-name").innerHTML = response.data.name;
-  document.querySelector("#current-temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#city-name");
+  let humidityElement = document.querySelector("#real-humidity");
+  let windElement = document.querySelector("#real-wind");
+  let iconElement = document.querySelector("#icon");
+  console.log(response);
+  farenheitTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(farenheitTemperature);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#current-temperature").innerHTML =
+    Math.round(farenheitTemperature);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -60,11 +70,34 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+function showFarenheitTemperature(event) {
+  event.preventDefault();
+  farenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let farenheitTemperature = document.querySelector("#current-temperature");
+  showTemp.innerHTML = Math.round(farenheitTemperature);
+}
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  farenheitLink.classList.remove("active");
+  let celsiusTemperature = ((farenheitTemperature - 32) * 5) / 9;
+  let showTemp = document.querySelector("#current-temperature");
+  showTemp.innerHTML = Math.round(celsiusTemperature);
+}
+
+let farenheitTemperature = null;
 
 let searchForm = document.querySelector("#search-city");
 searchForm.addEventListener("submit", handleSubmit);
 
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let farenheitLink = document.querySelector("#farenheit-link");
+farenheitLink.addEventListener("click", showFarenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
 
 searchCity("Portland");
